fe82ae0c336b333bfe859eb1e026505f948e0c63
BJF <skybjf@126.com>
2014-10-28 10:10:33 +0800

    添加第一个node.js测试例子

---
+++ nodejs/bjf.js
@@ -0,0 +1,7 @@
+var http=require("http");
+http.createServer(function(req,res){
+	res.writeHead(200,{'Content-Type': 'text/html'});
+	res.write('<h1>Hello 无趣</h1>');
+	res.end('<p>加油加油~~</p>');
+}).listen(3000);
+console.log("3000服务器已经被打开");
