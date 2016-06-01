ff65119cd4ff3fe8a5ea985ce69cdd8d9921e622
Jeffrey <skybjf@126.com>
2016-06-01 10:11:09 +0800

    requirejs练习

---
+++ require/js/hello.js
@@ -0,0 +1,9 @@
+function hello(){
+	console.log("hello.js");
+}
+function hello1(){
+	console.log("hello1.js");
+}
+function hello2(){
+	console.log("hello2.js");
+}
--- require/js/main.js
+++ require/js/main.js
@@ -1,14 +1,26 @@
 requirejs.config({
     baseUrl: 'js',
     paths: {
-        jquery:'jquery.min'
+        jquery: 'jquery.min',
+        hello: 'hello'
+    },
+    shim: {
+        hello: {
+            init: function() {
+                return {
+                    hello: hello,
+                    hello1: hello1,
+                    hello2: hello2
+                }
+            }
+        }
     },
     urlArgs: "v=" + new Date().getTime()
 });
 
-requirejs(["jquery","app"],function($,app) {
-    $(".btn").on("click",function(){
-        console.log(9999);    
+requirejs(["jquery", "app", "hello"], function($, app, hello) {
+    hello2();
+    $(".btn").on("click", function() {
+        app.sayHello();
     });
-    app.sayHello();
 });
